b8b88dcd19c58e08f4d8c6f5a5b7e8b01767fe93
zjbpku <zjbpku@gmail.com>
2021-07-10 21:10:08 +0800

    fix NPE on some devices

--- app/src/main/java/com/fidroid/skipper/DeviceHelper.kt
+++ app/src/main/java/com/fidroid/skipper/DeviceHelper.kt
@@ -6,19 +6,26 @@ import android.provider.Settings
 import android.text.TextUtils
 
 fun isAccessibilitySettingsEnabled(context: Context, clazz: Class<out AccessibilityService?>): Boolean {
-    var accessibilityEnabled = 0
-    val service: String = context.packageName.toString() + "/" + clazz.canonicalName
     try {
-        accessibilityEnabled = Settings.Secure.getInt(context.applicationContext.contentResolver, Settings.Secure.ACCESSIBILITY_ENABLED)
-    } catch (e: Settings.SettingNotFoundException) {
-        e.printStackTrace()
+        var accessibilityEnabled = 0
+        val service: String = context.packageName.toString() + "/" + clazz.canonicalName
+        try {
+            accessibilityEnabled = Settings.Secure.getInt(context.applicationContext.contentResolver, Settings.Secure.ACCESSIBILITY_ENABLED)
+        } catch (e: Settings.SettingNotFoundException) {
+            e.printStackTrace()
+        }
+        val mStringColonSplitter = TextUtils.SimpleStringSplitter(':')
+        if (accessibilityEnabled == 1) {
+            val settingValue: String = Settings.Secure.getString(context.applicationContext.contentResolver, Settings.Secure.ENABLED_ACCESSIBILITY_SERVICES)
+            if (settingValue.isNullOrEmpty()) {
+                return false
+            }
+            mStringColonSplitter.setString(settingValue)
+            return !mStringColonSplitter.find { it.lowercase() == service.lowercase() }.isNullOrEmpty()
+        }
+        return false
+    } catch (e: Exception) {
+        return false
     }
-    val mStringColonSplitter = TextUtils.SimpleStringSplitter(':')
-    if (accessibilityEnabled == 1) {
-        val settingValue: String = Settings.Secure.getString(context.applicationContext.contentResolver, Settings.Secure.ENABLED_ACCESSIBILITY_SERVICES)
-        mStringColonSplitter.setString(settingValue)
-        return !mStringColonSplitter.find { it.lowercase() == service.lowercase() }.isNullOrEmpty()
 
-    }
-    return false
 }
